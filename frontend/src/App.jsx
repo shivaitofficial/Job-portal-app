01a059218f8a53b6b36d140338a439e25edf01d1
shivaitofficial <shivaitofficial@gmail.com>
2026-04-29 17:45:18 +0530

    Login Phase : Added,Its Working Fine

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import RegisterPage from './RegisterPage'
+import LoginPage from './LoginPage';
 function App() {
 
   return (
     <>
       <div>
-        <RegisterPage/>
+     
+        <LoginPage/>
       </div>
     </>
   )
